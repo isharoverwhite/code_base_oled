156a126c15551d0d337015eee918bb98a8858b53
RYZEN <48115317+amdr5@users.noreply.github.com>
2022-02-16 04:50:53 -0800

    stat.py

--- oled.py
+++ oled.py
@@ -5,7 +5,7 @@ from board import SCL, SDA
 import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
- 
+import os
 
 i2c = busio.I2C(SCL, SDA)
  
@@ -38,34 +38,52 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
  
- 
+delay = 0
 # Load default font.
 font = ImageFont.load_default()
  
 while True:
- 
-    
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
- 
-    # Shell scripts for system monitoring from here:
-    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d' ' -f1"
-    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
-    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
- 
-    # Write four lines of text.
- 
-    draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
-    draw.text((x, top + 8), CPU, font=font, fill=255)
-    draw.text((x, top + 16), MemUsage, font=font, fill=255)
-    draw.text((x, top + 25), Disk, font=font, fill=255)
- 
-    # Display image.
-    disp.image(image)
-    disp.show()
-    time.sleep(0.1)
+                draw.rectangle((0, 0, width, height), outline=0, fill=0)
+                f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+                t = f.readline ()
+                cputemp = ""+t[0:2]
+                minitemp = t[3]
+                voltcore = 'vcgencmd measure_volts core'
+                volt = subprocess.check_output(voltcore, shell=True).decode("utf-8")
+                v = volt[5:9]
+                cmd = "hostname -I | cut -d' ' -f1"
+                IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+                cmd = 'df -h | awk \'$NF=="/"{printf "%s/%d", $3,$2,$5}\''
+                Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+                cmd = "free -m | awk 'NR==2{printf \"%s/%s\", $3,$2,$3*100/$2 }'"
+                MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+                cmd = 'cut -f 1 -d " " /proc/loadavg'
+                CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+                draw.rectangle((0, 0, width, height), outline=0, fill=0)
+                draw.text((0, top + 0), v+"V", font=font, fill=255)
+                draw.text((58, top + 0), "__", font=font, fill=255)
+                draw.text((58, top + 9), '__)', font=font, fill=255)
+                draw.text((0, top + 10), CPU, font=font, fill=255)
+                draw.text((58, top + 17), "__)" ,font=font, fill=255)
+                draw.text((0, top + 21), Disk+"", font=font, fill=255)
+                draw.text((80, top + 21), MemUsage, font=font, fill=255)
+                draw.text((100, top + 10), str(cputemp)+'°C', font=font, fill=255)
+                draw.text((57, top + 0), IP, font=font, fill=255)
+                if v <= '1.20':
+                    draw.text((40, top + 0), '!', font=font, fill=255)
+                if cputemp >= '45':
+                    draw.text((87, top + 0), '!', font=font, fill=255)
+                cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+                percent = subprocess.check_output(cmd, shell=True).decode("utf-8")
+                pdisk = percent[14:16]
+                if pdisk >= '80':
+                    draw.text((40, top + 21), '!', font=font, fill=255)
+                if CPU >= '5.00':
+                    draw.text((40, top + 10), '!', font=font, fill=255)
+                if pdisk >= '80' and v <= '1.20' and cputemp >= '45' and CPU >= '2.00' and MemUsage[0:3] >= '500':
+                    draw.text((76, top + 9), "!", font=font, fill=255)
+                if cputemp >='81':
+                    os.system("sudo shutdown now")
+                disp.image(image)
+                disp.show()
+                time.sleep(0.1)
